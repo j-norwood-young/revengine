812a67537e84dda5d02ebc256cf44b3d5d9b6608
Jason Norwood-Young <jason@10layer.com>
2020-06-18 14:43:54 +0200

    Pipeline model

---
+++ packages/api/models/pipeline_model.js
@@ -0,0 +1,26 @@
+/* global JXPSchema ObjectId Mixed */
+
+const PipelineSchema = new JXPSchema(
+    {
+        name: String,
+        pipeline: Mixed,
+        cron: String,
+        running: Boolean,
+        run_start: Date,
+        last_run_start: Date,
+        last_run_end: Date,
+        last_run_result: Mixed,
+    },
+    {
+        perms: {
+            admin: "crud", 
+            owner: "crud",
+            user: "r",
+            all: "" 
+        }
+    }
+);
+
+// Finally, we export our model. Make sure to change the name!
+const Pipeline = JXPSchema.model('Pipeline', PipelineSchema);
+module.exports = Pipeline;
