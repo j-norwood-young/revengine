af62dd9f126d833a1b3613bb88c3a3c7462f2a48
Jason Norwood-Young <jason@10layer.com>
2020-07-01 09:19:04 +0200

    New link format

--- packages/api/models/test_model.js
+++ packages/api/models/test_model.js
@@ -7,9 +7,9 @@ const TestSchema = new JXPSchema({
     shmack: [String], // We can store arrays
     password: String, // Passwords are automagically encrypted
     fulltext: { type: String, index: { text: true } },
-    link_id: { type: ObjectId, link: "link", }, // We can populate these links during a query
-    other_link_id: { type: ObjectId, link: "link", map_to: "other_link" },
-    array_link_id: [{ type: ObjectId, link: "link", map_to: "array_link" } ], // TODO
+    link_id: { type: ObjectId, link: "Link", }, // We can populate these links during a query
+    other_link_id: { type: ObjectId, link: "Link", map_to: "other_link" },
+    array_link_id: [{ type: ObjectId, link: "Link", map_to: "array_link" } ], // TODO
 },
 {
     perms: {
