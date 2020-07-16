45ea0121bd5389f90e131e9525d6a7ac8cc1493d
Jason Norwood-Young <jason@10layer.com>
2020-07-16 15:50:04 +0200

    Model to track hits by day

---
+++ packages/api/models/dailyhit_model.js
@@ -0,0 +1,20 @@
+/* global JXPSchema */
+
+const DailyHitSchema = new JXPSchema({
+    article_id: Number,
+    urlid: String,
+    date: Date,
+    hits: Number,
+    logged_in_hits: Number
+},
+{
+    perms: {
+        admin: "crud",
+        owner: "crud",
+        user: "cr",
+        all: ""
+    }
+});
+
+const DailyHit = JXPSchema.model('DailyHit', DailyHitSchema);
+module.exports = DailyHit;
