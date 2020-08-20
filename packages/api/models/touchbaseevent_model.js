c95fe079c4944fcc0a73b72307f222eb00a37abc
Jason Norwood-Young <jason@10layer.com>
2020-08-20 11:14:43 +0200

    Touchbase Event odel

---
+++ packages/api/models/touchbaseevent_model.js
@@ -0,0 +1,27 @@
+/* global JXPSchema ObjectId Mixed */
+
+const TouchbaseEventSchema = new JXPSchema({
+    event: String,
+    email: String,
+    url: String,
+    touchbase_campaign_id: String,
+    touchbase_list_id: String,
+    campaign_id: { type: ObjectId, link: "touchbasecampaign"},
+    list_id: { type: ObjectId, link: "touchbaselist" },
+    timestamp: Date,
+    ip_address: String,
+    lattitude: String,
+    longitude: String,
+    city: String,
+    region: String,
+    country_code: String,
+    country_name: String
+},
+{
+    perms: {
+        admin: "crud",
+    }
+});
+
+const TouchbaseEvent = JXPSchema.model('TouchbaseEvent', TouchbaseEventSchema);
+module.exports = TouchbaseEvent;
