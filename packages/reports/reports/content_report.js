f04a67002723bcb717a7c56dc7ce40b5cce6cf6c
Jason Norwood-Young <jason@10layer.com>
2021-10-21 16:19:32 +0200

    Send to PDF for now until we can sort shit out

--- packages/reports/reports/content_report.js
+++ packages/reports/reports/content_report.js
@@ -5,14 +5,15 @@ require("dotenv").config();
 const content = async (params = {}) => {
     try {
         if (!params.scheduled_report_id) return "?scheduled_report_id=<scheduled_report_id> required";
+        const fname = `/tmp/${params.scheduled_report_id}.pdf`
         const url = `${config.frontend.url}report/editorial_dashboard_mail?scheduled_report_id=${params.scheduled_report_id}&mail_view=1&apikey=${process.env.APIKEY}&user_id=${process.env.USER_ID}`
         console.log(url)
         const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
         const page = await browser.newPage();
         await page.goto(url)
         await page.waitForNetworkIdle();
-        const content = await page.content();
-        return content;
+        await page.pdf({ path: fname });
+        return "PDF generated";
     } catch(err) {
         console.error(err);
         return JSON.stringify(err)
