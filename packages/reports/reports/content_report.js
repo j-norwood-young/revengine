be01ae34c98a9ea297025cd6e20cf15f073c71c6
Jason Norwood-Young <jason@10layer.com>
2021-10-22 14:28:42 +0200

    Make sure we shut down the browser even if there's a fuckup

--- packages/reports/reports/content_report.js
+++ packages/reports/reports/content_report.js
@@ -7,12 +7,13 @@ const moment = require("moment")
 const numberFormat = new Intl.NumberFormat(config.locale || "en-GB", { maximumFractionDigits: 1 });
 
 const content = async (params = {}) => {
+    let browser;
     try {
         if (!params.scheduled_report_id) return "?scheduled_report_id=<scheduled_report_id> required";
         const fname = `/tmp/${params.scheduled_report_id}.pdf`
         const url = `${config.frontend.url}report/editorial_dashboard_mail?scheduled_report_id=${params.scheduled_report_id}&mail_view=1&apikey=${process.env.APIKEY}&user_id=${process.env.USER_ID}`
         console.log(url)
-        const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+        browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
         const page = await browser.newPage();
         await page.goto(url)
         await page.waitForSelector("#loaded");
@@ -22,6 +23,7 @@ const content = async (params = {}) => {
         const template = pug.compileFile(path.join(__dirname, "../templates/content_report.pug"));
         return template({ content, moment, numberFormat })
     } catch(err) {
+        if (browser) browser.close();
         console.error(err);
         return JSON.stringify(err)
     }
