acf06969ca034f5661ca413556dad5a74cd14a04
Jason Norwood-Young <jason@10layer.com>
2021-12-01 11:39:21 +0100

    Changes to /top_articles/:period - published_date_gte - unfiltered_fallback

--- packages/wordpress_api/index.js
+++ packages/wordpress_api/index.js
@@ -17,13 +17,83 @@ const cors = Cors({
 server.pre(cors.preflight);
 server.use(cors.actual);
 
+/**
+ * {post} /top_articles/:period?params
+ * 
+ * @param (String) "hour", "day", "week", "month"
+ * 
+ * POST params:
+ * - size: (Int) number of posts to return, default: 5
+ * - published_date_gte: (Date) return posts published on or after this date
+ * - unfiltered_fallback: (Boolean) return unfiltered posts if less than ${size} results are found
+ * - article_id: (Int) return a single post by id
+ * - author: (String) return posts by author
+ * - content_type: (String) return posts by content type
+ * - tag: (String) return posts by tag
+ * - section: (String) return posts by section
+ * - exclude_section: (String) return posts by section, excluding the given section
+ * 
+ * returns:
+ * (Array) array of posts
+ * {
+        "_id": (ObjectId) article._id,
+        "post_id": (Int) article.post_id,
+        "author": (String) article.author,
+        "date_modified": (String) article.date_modified,
+        "date_published": (String) article.date_published,
+        "sections": (Array) (String) article.sections,
+        "tags": (Array) (String) article.tags,
+        "title": (String) article.title,
+        "urlid": (String) article.urlid,
+        "img_full": (String) article.img_full,
+        "img_medium": (String) article.img_medium,
+        "img_thumbnail": (String) article.img_thumbnail,
+        "custom_section_label": (String) article.custom_section_label,
+        "hits": (Int) article.hits,
+    },
+ *
+ * Example:
+ * /top_articles/day?size=5&section=South+Africa&tag=Table+Mountain&published_date_gte=2021-04-01&unfiltered_fallback=1
+ * Returns: 
+ * [
+    {
+        "_id": "607c13a11c234ab10e9ca296",
+        "post_id": 895227,
+        "author": "Tiara Walters",
+        "date_modified": "2021-04-18T18:26:39.000Z",
+        "date_published": "2021-04-18T11:09:48.000Z",
+        "sections": [
+            "Our Burning Planet",
+            "South Africa"
+        ],
+        "tags": [
+            "Devil’s Peak",
+            "Fire",
+            "NCC Wildfires",
+            "Philip Kgosana Drive",
+            "Rhodes Memorial",
+            "South African National Parks",
+            "Table Mountain"
+        ],
+        "title": "‘Out-of-control’ Table Mountain fire forces UCT evacuation",
+        "urlid": "pyrocene-cape-out-of-control-wildfire-rages-on-slopes-of-table-mountain",
+        "img_full": "https://www.dailymaverick.co.za/wp-content/uploads/fire-pic.jpeg",
+        "img_medium": "https://www.dailymaverick.co.za/wp-content/uploads/fire-pic-480x360.jpeg",
+        "img_thumbnail": "https://www.dailymaverick.co.za/wp-content/uploads/fire-pic-150x150.jpeg",
+        "custom_section_label": "Pyrocene Cape II",
+        "hits": 4
+    },
+    ...
+    ]
+ **/
 server.get("/top_articles/:period", async (req, res) => {
     try {
         const report = new Reports.TopLastPeriod();
-        const params = Object.assign({ size: 5 }, req.params, req.query);
+        const size = req.query.size || req.params.size || 5;
+        const params = Object.assign({ size }, req.params, req.query);
         // console.log(params);
         const top_articles = await report.run(params);
-        const articles = (await jxphelper.aggregate("article", [
+        let articles = (await jxphelper.aggregate("article", [
             {
                 $match: {
                     post_id: { $in: top_articles.map(a => a.key) }
@@ -51,7 +121,14 @@ server.get("/top_articles/:period", async (req, res) => {
             article.hits = top_articles.find(hit => hit.key === article.post_id).doc_count;
         }
         articles.sort((a, b) => b.hits - a.hits);
-        res.send(articles);
+        let filtered_articles = articles;
+        if (params.published_date_gte) {
+            filtered_articles = articles.filter(a => +new Date(a.date_published) >= +new Date(params.published_date_gte));
+        }
+        if (params.unfiltered_fallback && filtered_articles.length < size) {
+            filtered_articles = articles;
+        }
+        res.send(filtered_articles.slice(0, size));
     } catch(err) {
         console.error(err);
         res.send(500, { status: "error", error: err });
