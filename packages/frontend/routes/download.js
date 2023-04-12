d169a205c3104bb92d1323059ba2dd96735a17b5
Jason Norwood-Young <jason@10layer.com>
2023-04-12 13:29:01 +0200

    Autologin downloads - back end

--- packages/frontend/routes/download.js
+++ packages/frontend/routes/download.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const config = require("config");
 const moment = require("moment");
+const encrypt = require("@revengine/wordpress_auth").encrypt;
 
 const opts = {
     "fields": "email,first_name,last_name,createdAt,updatedAt,label_id,segmentation_id,cc_expiry_date,cc_last4_digits,wordpress_id,paying_customer"
@@ -47,5 +48,36 @@ router.get("/label/csv/:label_id", async (req, res) => {
         res.send(err);
     }
 })
+
+router.get("/label/autologins/:label_id", async (req, res) => {
+    try {
+        const label = (await req.apihelper.getOne("label", req.params.label_id)).data;
+        const readers = (await req.apihelper.get("reader", { "filter[label_id]": req.params.label_id, "fields": "email,wordpress_id" })).data;
+        const data = [["email", "autologin"]];
+        for (const reader of readers) {
+            if (!reader.wordpress_id) continue;
+            const token = encrypt({
+                "wordpress_id": reader.wordpress_id,
+                "revengine_id": reader._id,
+                "email": reader.email,
+            })
+            data.push([
+                reader.email,
+                token
+            ])
+        }
+        console.log(data);
+        const csv = require("csv-stringify");
+        csv(data, (err, output) => {
+            if (err) throw err;
+            
+            res.attachment(`${label.name.toLowerCase().replace(" ", "_")}-autologins-${moment().format("YYYY-MM-DDTHH:mm:ss")}.csv`).send(output);
+        })
+    } catch(err) {
+        console.error(err);
+        res.send(err);
+    }
+})
+
 
 module.exports = router;
