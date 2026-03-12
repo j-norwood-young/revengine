5f6344b7dc14142545ffd1dfef7ca86c7a1144fd
Jason <jason@10layer.com>
2026-03-12 14:49:49 +0100

    Fix reports and add savedchart_model

--- packages/api/models/report_model.js
+++ packages/api/models/report_model.js
@@ -9,33 +9,46 @@ const ReportChartItemSchema = new JXPSchema({
 });
 
 const ReportScheduleSchema = new JXPSchema({
-    frequency: String,
+    frequency: {
+        type: String,
+        enum: ["daily", "weekly", "monthly", "quarterly"],
+    },
     dayOfWeek: Number,
     dayOfMonth: Number,
     time: String,
     recipients: [String],
-    format: String,
+    format: {
+        type: String,
+        enum: ["pdf", "excel", "csv"],
+    },
 });
 
-const ReportSchema = new JXPSchema({
-    name: { type: String, required: true },
-    description: String,
-    type: { type: String, index: true },
-    charts: [ReportChartItemSchema],
-    is_public: { type: Boolean, default: false },
-    schedule: ReportScheduleSchema,
-    created_at: { type: Date, default: Date.now },
-    updated_at: { type: Date, default: Date.now },
-    created_by: String,
-},
-{
-    perms: {
-        admin: "crud",
-        owner: "crud",
-        user: "r",
-        all: ""
+const ReportSchema = new JXPSchema(
+    {
+        name: { type: String, required: true, index: true },
+        description: String,
+        type: {
+            type: String,
+            enum: ["reader", "revenue", "engagement", "segmentation", "custom"],
+            default: "custom",
+            index: true,
+        },
+        charts: [ReportChartItemSchema],
+        is_public: { type: Boolean, default: false },
+        schedule: ReportScheduleSchema,
+        created_at: { type: Date, default: Date.now },
+        updated_at: { type: Date, default: Date.now },
+        created_by: { type: ObjectId, link: "user" },
+    },
+    {
+        perms: {
+            admin: "crud",
+            owner: "crud",
+            user: "r",
+            all: "",
+        },
     }
-});
+);
 
 ReportSchema.pre("save", function (next) {
     this.updated_at = new Date();
@@ -45,6 +58,6 @@ ReportSchema.pre("save", function (next) {
     next();
 });
 
-const Report = JXPSchema.model("Report", ReportSchema);
+const Report = JXPSchema.model("report", ReportSchema);
 module.exports = Report;
 
